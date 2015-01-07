73210dfd7e2a78f382314ac60c1c51919f243547
dong <ddliuhb@gmail.com>
2015-01-08 00:01:09 +0800

    cli

--- spider/main.go
+++ spider/main.go
@@ -6,27 +6,85 @@ package main
 import (
     "flag"
     "time"
+    "strings"
+    "os"
     "github.com/ddliu/go-spider"
     "github.com/ddliu/go-spider/pipes"
+    "github.com/codegangsta/cli"
 )
 
 func main() {
-    // enable logging...
-    flag.Parse()
+    app := cli.NewApp()
+    app.Name = "spider"
+    app.Usage = "download links"
 
-    s := spider.NewSpider()
-
-    s.Concurrency = 3
-
-    s.
-        Pipe(func(s *spider.Spider, t *spider.Task) {
-            time.Sleep(1000 * time.Millisecond)
-        }).
-        Pipe(pipes.Print)
-
-    for i := 0; i < 1000; i++ {
-        s.AddUri("http://google.com/").AddUri("http://github.com/")
+    app.Flags = []cli.Flag {
+        cli.StringFlag {
+            Name: "download, d",
+            Value: "*",
+            Usage: "which url to download",
+        },
+        cli.StringFlag {
+            Name: "follow",
+            Value: "*",
+            Usage: "which url to follow",
+        },
+        cli.StringFlag {
+            Name: "target, t",
+            Value: ".",
+            Usage: "target folder to save downloaded files",
+        },
+        cli.IntFlag {
+            Name: "concurrency, c",
+            Value: 1,
+            Usage: "spider concurrency",
+        },
     }
-        
-    s.Run()
+
+    app.Action = func(c *cli.Context) {
+        var downloadPath = c.String("target")
+        s := spider.NewSpider().
+            Pipe(pipes.NormalizeUrl).
+            Pipe(pipes.IfUrl(c.String("download"), pipes.Download(func(uri string) string {
+                return downloadPath + "/" + Path(uri)
+            }, 0777), nil))
+            Pipe(pipes.IfUrl(c.String("follow"), nil, pipes.Ignore))
+
+
+        c.Args()
+    }
+
+    app.Run(os.Args)
+}
+
+// Makes a string safe to use as an url path, cleaned of .. and unsuitable characters
+// see: https://github.com/kennygrant/sanitize/blob/master/sanitize.go
+func Path(text string) string {
+    // Start with lowercase string
+    fileName := strings.ToLower(text)
+    fileName = strings.Replace(fileName, "..", "", -1)
+    fileName = path.Clean(fileName)
+    fileName = strings.Trim(fileName, " ")
+
+    // Replace certain joining characters with a dash
+    seps, err := regexp.Compile(`[ &_=+:]`)
+    if err == nil {
+        fileName = seps.ReplaceAllString(fileName, "-")
+    }
+
+    // Flatten accents first
+    fileName = Accents(fileName)
+
+    // Remove all other unrecognised characters
+    // we are very restrictive as this is intended for ascii url slugs
+    legal, err := regexp.Compile(`[^\w\_\~\-\./]`)
+    if err == nil {
+        fileName = legal.ReplaceAllString(fileName, "")
+    }
+
+    // Remove any double dashes caused by existing - in name
+    fileName = strings.Replace(fileName, "--", "-", -1)
+
+    // NB this may be of length 0, caller must check
+    return fileName
 }
